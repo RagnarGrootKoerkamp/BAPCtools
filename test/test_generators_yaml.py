77ae7760c192367833bbf6632da572d0db688e3c
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-06-18 00:05:26 +0200

    Add pytest tests for bad_generators.yaml

---
+++ test/test_generators_yaml.py
@@ -0,0 +1,23 @@
+import pytest
+import yaml
+from pathlib import Path
+
+import generate
+
+class MockProblem:
+    def __init__(self):
+        self.path = Path('.')
+        self._program_callbacks = dict()
+        self._rules_cache = dict()
+
+class MockGeneratorConfig(generate.GeneratorConfig):
+    def __init__(self, problem):
+        self.problem=problem
+
+def test_bad_generators_yamls():
+    docs = yaml.load_all(Path('test/generator_yaml/bad_generator.yaml').read_text(), Loader=yaml.SafeLoader)
+    for doc in docs:
+        print(doc)
+        with pytest.raises(SystemExit) as e:
+            MockGeneratorConfig(MockProblem()).parse_yaml(doc)
+
