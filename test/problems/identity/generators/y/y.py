7e6e9f94c160a9d0544ba5d53c3f8cb0f9cd45a9
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-06-18 23:47:28 +0200

    Make generator dependent on directory name, not filename.

--- test/problems/identity/generators/y/y.py
+++ test/problems/identity/generators/y/y.py
@@ -2,5 +2,5 @@ import sys
 import random
 import pathlib
 # Seed with the directory name.
-random.seed(pathlib.Path(sys.argv[0]).parts[-1])
+random.seed(pathlib.Path(sys.argv[0]).parts[-2])
 print(random.randint(0, 1000))
