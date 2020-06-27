11c7d27ca5d44d134cbc124493cd50e62aa36915
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-06-27 11:22:39 +0200

    Fix test-memsize.cc to quit faster.

---
+++ test/problems/hello/submissions/run_time_error/test-memsize.cc
@@ -0,0 +1,28 @@
+/*
+ * This should fail with RUN-ERROR due to running out of memory, which
+ * is restricted.
+ *
+ * @EXPECTED_RESULTS@: RUN-ERROR
+ */
+
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<vector<char>> vs;
+	while(true) {
+		vector<char> v;
+		// Allocate 2GB at a time.
+		try {
+			v.reserve(1ll * 1024 * 1024);
+			vs.push_back(std::move(v));
+		} catch(const std::bad_alloc&) {
+			// Handle the exception and clean-up manually to prevent slow termination of the program.
+			vs.~vector<vector<char>>();
+			// Make sure to re-raise the exit with SIGABRT.
+			abort();
+		}
+	}
+	return 0;
+}
