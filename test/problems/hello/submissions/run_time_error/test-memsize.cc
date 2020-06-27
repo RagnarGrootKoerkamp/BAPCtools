a33708adca5c5a9284c47d57a1becc323a24ea01
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-06-27 12:01:05 +0200

    Update comment

--- test/problems/hello/submissions/run_time_error/test-memsize.cc
+++ test/problems/hello/submissions/run_time_error/test-memsize.cc
@@ -5,6 +5,7 @@
  * @EXPECTED_RESULTS@: RUN-ERROR
  */
 
+#include <cstdlib>
 #include <vector>
 
 using namespace std;
@@ -13,12 +14,13 @@ int main() {
 	vector<vector<char>> vs;
 	while(true) {
 		vector<char> v;
-		// Allocate 2GB at a time.
+		// Allocate 1MB at a time.
 		try {
-			v.reserve(1ll * 1024 * 1024);
+			v.reserve(1024 * 1024);
 			vs.push_back(std::move(v));
 		} catch(const std::bad_alloc&) {
-			// Handle the exception and clean-up manually to prevent slow termination of the program.
+			// Handle the exception and clean-up manually to prevent slow termination of the
+			// program.
 			vs.~vector<vector<char>>();
 			// Make sure to re-raise the exit with SIGABRT.
 			abort();
