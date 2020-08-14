e27c304e0d746ca1cc4674a49001ee1a3057ff45
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-08-14 11:46:15 +0200

    [build] Fix missing `testcases` argument.

--- bin/tools.py
+++ bin/tools.py
@@ -546,6 +546,7 @@ def run_parsed_arguments(args):
             config.args.jobs = 4
             config.args.add_manual = False
             config.args.move_manual = False
+            config.args.testcases = None
             success &= generate.generate(problem)
         if action in ['validate', 'input', 'all']:
             success &= problem.validate_format('input_format')
