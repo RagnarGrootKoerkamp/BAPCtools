87e7815a1328ffa11eb66a7a7b93ddd5ddae5429
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-08-23 10:19:22 +0200

    Fix unknown config.args.testcases key bug.

--- bin/tools.py
+++ bin/tools.py
@@ -546,10 +546,9 @@ def run_parsed_arguments(args):
             config.args.jobs = 4
             config.args.add_manual = False
             config.args.move_manual = False
-            tmp = config.args.testcases
-            config.args.testcases = None
+            if not hasattr(config.args, 'testcases'):
+                config.args.testcases = None
             success &= generate.generate(problem)
-            config.args.testcases = tmp
         if action in ['validate', 'input', 'all']:
             success &= problem.validate_format('input_format')
         if action in ['validate', 'output', 'all']:
