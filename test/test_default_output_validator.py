1e2b4eedfc044aa0bd260953bfc6aaff07054da0
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-08-26 16:58:52 +0200

    [tests/default output validator] #68: add a bunch of tests for default output validators.

---
+++ test/test_default_output_validator.py
@@ -0,0 +1,93 @@
+import pytest
+import argparse
+import yaml
+import os
+import hashlib 
+import tempfile
+from pathlib import Path
+
+import problem
+import run
+import validate
+import util
+import config
+
+RUN_DIR = Path.cwd().resolve()
+# Note: the python version isn't tested by default, because it's quite slow.
+DEFAULT_OUTPUT_VALIDATORS = ['default_output_validator.cpp']
+
+config.args = argparse.Namespace()
+config.args.cpp_flags = None
+config.args.verbose = 2
+config.args.force_build = False
+config.args.error = True
+
+# return list of (flags, ans, out, expected result)
+def read_tests():
+    docs = yaml.load_all((RUN_DIR / 'test/default_output_validator/default_output_validator.yaml').read_text(), Loader=yaml.SafeLoader)
+
+    tests = []
+
+    for doc in docs:
+        doc['ans'] = str(doc['ans'])
+        if 'ac' in doc:
+            for out in doc['ac']: tests.append((doc['flags'], doc['ans'], str(out), True))
+        if 'wa' in doc:
+            for out in doc['wa']: tests.append((doc['flags'], doc['ans'], str(out), 43))
+
+    print(tests)
+    return tests
+
+@pytest.fixture(scope='class', params=DEFAULT_OUTPUT_VALIDATORS)
+def validator(request):
+    problem_dir = RUN_DIR / 'test/problems/identity'
+    os.chdir(problem_dir)
+
+    h = hashlib.sha256(bytes(Path().cwd())).hexdigest()[-6:]
+    tmpdir = Path(tempfile.gettempdir()) / ('bapctools_' + h)
+    tmpdir.mkdir(exist_ok=True)
+    p = problem.Problem(Path('.'), tmpdir)
+    validator = validate.OutputValidator(p, RUN_DIR  / 'support' / request.param)
+    print(util.ProgressBar.current_bar)
+    bar = util.ProgressBar('build', max_len=1)
+    validator.build(bar)
+    bar.finalize()
+    yield (p, validator)
+    os.chdir(RUN_DIR)
+
+class MockRun():
+    pass
+
+@pytest.mark.usefixtures('validator')
+class TestDefaultOutputValidators:
+    @pytest.mark.parametrize( 'testdata', read_tests())
+    def test_default_output_validators(self, validator, testdata):
+        problem, validator = validator
+        flags, ans, out, exp = testdata
+        flags = flags.split()
+
+        (problem.tmpdir/'data').mkdir(exist_ok=True,parents=True)
+        in_path = problem.tmpdir / 'data/test.in'
+        ans_path = problem.tmpdir / 'data/test.ans'
+        out_path = problem.tmpdir / 'data/test.out'
+        ans_path.write_text(ans)
+        out_path.write_text(out)
+
+        in_path.write_text('')
+
+        t = run.Testcase(problem, in_path, short_path = Path('test'))
+        r = MockRun()
+        r.feedbackdir = problem.tmpdir / 'data'
+        r.out_path = out_path
+
+        problem.settings.validator_flags = flags
+
+        result = validator.run(t, r)
+        if result.ok != exp:
+            print(testdata)
+            for k in vars(result):
+                print(k, " -> ", getattr(result,k))
+        assert result.ok == exp
+
+
+
