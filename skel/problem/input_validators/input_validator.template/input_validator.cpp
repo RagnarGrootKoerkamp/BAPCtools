3d777a99c12def8ea2a3f2b37f784e1044ef5fa4
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2021-10-02 00:56:41 +0300

    move todo

--- skel/problem/input_validators/input_validator.template/input_validator.cpp
+++ skel/problem/input_validators/input_validator.template/input_validator.cpp
@@ -3,7 +3,6 @@
 // Check the grammar of the input files.
 // You should also check properties of the input.
 // E.g., check that a graph is connected.
-// TODO: Remove this comment, and summarize your input validator.
 
 int main(int argc, char** argv) {
 	InputValidator v(argc, argv);
@@ -19,9 +18,10 @@ int main(int argc, char** argv) {
 	v.read_integers("v", /*count=*/10, 0, 1000000, Unique);
 	v.test_string("ACCEPTED"); // only succeeds when it reads the given string.
 	v.read_string("s", 4, 5);     // only succeeds when it reads a string with length in inclusive range.
-	bool b = v.peek('x');    // test the next character.
+	bool b = v.peek('x'); // test the next character.
 	v.WA("The input is not valid."); // Print error and exit with code 43.
 	v.check(false, "WA on false");
 
 	// In its destructor, v automatically exits with code 42 here.
+	// TODO: Remove this comment, and summarize your input validator.
 }
