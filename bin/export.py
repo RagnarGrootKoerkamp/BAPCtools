b9b6ee7b836e2a7be0f6e88942196776c4953288
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2021-10-29 01:42:48 +0200

    zip: also zip .{in,ans}.statement

--- bin/export.py
+++ bin/export.py
@@ -109,6 +109,8 @@ def build_problem_zip(problem, output, settings):
             ('problem_statement/*', True),
             # Either .interaction or .in.statement should be present, but we only care about .interaction here.
             ('data/sample/*.interaction', False),
+            ('data/sample/*.in.statement', False),
+            ('data/sample/*.ans.statement', False),
             ('data/secret/*.in', True),
             # Not really needed, but otherwise problemtools will complain.
             ('data/secret/*.ans', True),
