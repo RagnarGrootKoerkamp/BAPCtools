5cbda854550446e1e069f21d135946164594a855
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-06-30 07:24:24 +0200

    Add new hellounix test problem aimed at unixy systems

---
+++ test/problems/hellounix/submissions/run_time_error/test-sigfpe.c
@@ -0,0 +1,18 @@
+/*
+ * This should fail with RUN-ERROR due to integer division by zero,
+ * giving an exitcode 136.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b;
+
+	b = 10 / a;
+
+	printf("%d\n",b);
+
+	return 0;
+}
---
+++ test/problems/hellounix/submissions/run_time_error/test-sigsegv.c
@@ -0,0 +1,15 @@
+/*
+ * This should fail with RUN-ERROR due to a segmentation fault,
+ * giving an exitcode 139.
+ */
+
+int main()
+{
+	int a[10];
+	int *b;
+
+	b = 10;
+	*b = a[-1000000];
+
+	return 0;
+}
