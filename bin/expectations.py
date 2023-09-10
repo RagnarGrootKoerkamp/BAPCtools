5f7a2bd2be7eb6a7a0dced8b76b83c3103c26003
Thore Husfeldt <thore.husfeldt@gmail.com>
2023-09-10 16:35:38 +0200

    Correct semantics of required (some, not all, must exist)
    Also introduce shortform like AC for ACCEPTED

--- bin/expectations.py
+++ bin/expectations.py
@@ -13,7 +13,7 @@ Here is a sample expectations.yaml file:
 A yaml parser will turn this into a dict that can be fed to the Registry class:
 
 >>> exp_dict = {
-...     "accepted/": "accepted", 
+...     "accepted/": "accepted",
 ...     "wrong_answer/th.py": {"sample": "accepted", "secret": "wrong answer"},
 ...     "mixed/failing.java": {"secret/huge/graph07": {"allowed": ["TLE", "RTE"]}}
 ... }
@@ -67,12 +67,26 @@ score
     NOT IMPLEMENTED
 
 range
-    A string of two space-separated numbers, like '0 30' or '-inf 43' or '3.14 3.14'; 
+    A string of two space-separated numbers, like '0 30' or '-inf 43' or '3.14 3.14';
     a one-value range can be abbreviated: '5' is the range '5 5'.
     NOT IMPLEMENTED
 """
 
 from functools import lru_cache
+
+
+@staticmethod
+def shortform(verdict):
+    """Transform verbose forms like ACCEPTED to short like AC, if needed."""
+    for long, short in [
+        ("ACCEPTED", "AC"),
+        ("WRONG_ANSWER", "WA"),
+        ("TIME_LIMIT_EXCEEDED", "TLE"),
+        ("RUN_TIME_ERROR", "RTE"),
+    ]:
+        if verdict == long:
+            verdict = short
+    return verdict
 
 
 class Expectations:
@@ -90,12 +104,12 @@ class Expectations:
     False
     >>> unexpected_results = {"sample/1": "AC", "secret/1": "AC", "secret/2": "AC"}
     >>> expected_results = {"sample/1": "AC", "secret/1": "AC", "secret/2": "WA"}
-    >>> missing = e.missing_required_verdics(unexpected_results)
-    >>> missing[""]
+    >>> missing = e.missing_required_verdicts(unexpected_results)
+    >>> missing['']
     {'WA'}
-    >>> missing = e.missing_required_verdics(expected_results)
-    >>> missing[""]
-    set()
+    >>> missing = e.missing_required_verdicts(expected_results)
+    >>> missing
+    {}
     >>> (e.is_satisfied_by(expected_results), e.is_satisfied_by(unexpected_results))
     (True, False)
 
@@ -147,15 +161,15 @@ class Expectations:
             elif isinstance(expectations, list):
                 pass  # NOT IMPLEMENTED
             elif isinstance(expectations, dict):
-                for k, v in expectations.items():
+                for k, val in expectations.items():
                     if k.startswith("sample") or k.startswith("secret"):
                         if pattern != "":
                             assert False  # only allowed on top level!
-                        parse_expectations(k, v)
+                        parse_expectations(k, val)
                     elif k == "allowed":
-                        self._allowed_verdicts[pattern] = v if isinstance(v, set) else set(v)
+                        self._allowed_verdicts[pattern] = val if isinstance(val, set) else set(val)
                     elif k == "required":
-                        self._required_verdicts[pattern] = v if isinstance(v, set) else set(v)
+                        self._required_verdicts[pattern] = val if isinstance(val, set) else set(val)
                     elif k in ["judge_message", "score", "fractional_score"]:
                         pass  # NOT IMPLEMENTED
                     else:
@@ -165,73 +179,75 @@ class Expectations:
 
     def is_allowed_verdict(self, path, verdict: str):
         """Is the result allowed for the testcase at the given path?"""
-        for long, short in [
-                ("ACCEPTED", "AC"),
-                ("WRONG_ANSWER", "WA"),
-                ("TIME_LIMIT_EXCEEDED", "TLE"),
-                ("RUN_TIME_ERROR", "RTE")
-                ]:
-            if verdict == long:
-                verdict = short
-
+        verdict = shortform(verdict)
         for pattern, allowed in self._allowed_verdicts.items():
             if path.startswith(pattern) and verdict not in allowed:
                 return False
         return True
 
-    def missing_required_verdics(self, results: dict[str, str]) -> dict[str, set[str]]:
-        """Which verdicts are missing?"""
+    def missing_required_verdicts(
+        self, verdict_for_testcase: dict[str, str]
+    ) -> dict[str, set[str]]:
+        """Which verdicts are missing?
 
-        return {
-            pattern: set(
-                required_verdict
-                for required_verdict in self._required_verdicts[pattern]
-                if all(
-                    results[path] != required_verdict
-                    for path in results
-                    if path.startswith(pattern)
-                )
-            )
-            for pattern in self._required_verdicts
-        }
+        Returns a map of expectation patterns to sets of verdicts.
+
+        >>> e = Expectations("does not terminate")
+        >>> results = {"sample/1": "AC", "secret/1": "AC", "secret/2": "WA"}
+        >>> e.missing_required_verdicts(results) ==  {'': {'RTE', 'TLE'}}
+        True
+        >>> results = {"sample/1": "AC", "secret/1": "TLE", "secret/2": "WA"}
+        >>> e.missing_required_verdicts(results)
+        {}
+        """
+
+        missing = dict()
+        for pattern, required_verdicts in self._required_verdicts.items():
+            for tc, verdict in verdict_for_testcase.items():
+                verdict = shortform(verdict)
+                if tc.startswith(pattern) and verdict in required_verdicts:
+                    break
+            else:
+                missing[pattern] = required_verdicts
+        return missing
 
     def is_satisfied_by(self, results: dict[str, str]) -> bool:
         """Are all requirements satisfied?"""
-        missing = self.missing_required_verdics(results)
+        missing = self.missing_required_verdicts(results)
         return all(self.is_allowed_verdict(path, results[path]) for path in results) and all(
             not missing_verdict for missing_verdict in missing.values()
         )
 
-class Registry:
-    """ Maps string that describe submissions to Expectation objects. """
-    def __init__(self, registry):
-        self.registry = { pat: Expectations(registry[pat]) for pat in registry }
 
+class Registry:
+    """Maps string that describe submissions to Expectation objects."""
+
+    def __init__(self, registry):
+        self.registry = {pat: Expectations(registry[pat]) for pat in registry}
 
     def __str__(self):
         return str(self.registry)
 
     @lru_cache
     def expectations(self, submission_path):
-        """ The expectations for a given submission. 
-            *Should* return the most specific match. (Currently assumes
-            there's exactly one.)
+        """The expectations for a given submission.
+        *Should* return the most specific match. (Currently assumes
+        there's exactly one.)
         """
         expectations = None
         for pat, exp in self.registry.items():
             if submission_path.startswith(pat):
                 if expectations is not None:
-                    assert False # NOT IMPLEMENTED: every pattern can match at most once
+                    assert False  # NOT IMPLEMENTED: every pattern can match at most once
                 expectations = exp
         if expectations is None:
-            assert False # NOT IMPLEMENTED: every submission must match
+            assert False  # NOT IMPLEMENTED: every submission must match
         return expectations
 
     def check_submission(self, submission_path: str, results) -> bool:
-        """ Check that given results were expected for the submission at the given path. """
+        """Check that given results were expected for the submission at the given path."""
         expectations = self.expectations(submission_path)
         return expectations.is_satisfied_by(results)
-
 
 
 if __name__ == "__main__":
