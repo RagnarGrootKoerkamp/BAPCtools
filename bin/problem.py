b7ea98c1132059f9f58d16207829f42b6b845322
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-01-15 15:45:16 +0100

    Match \problemyamlname directly

--- bin/problem.py
+++ bin/problem.py
@@ -80,7 +80,7 @@ class Problem:
                     case None:
                         error(rf"No \problemname found in problem.{lang}.tex")
                         continue
-                    case '\problemname':
+                    case r'\\problemyamlmname':
                         continue
                     case s if '\\' in s or '_' in s or '^' in s:
                         # texname contains markup, like "CO_2" or "\emph{Hello}":
