494a867049d5cc8fe96d2576e16e68a342dfc1d5
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-01-17 19:00:44 +0100

    boolfind: even better input validator

--- test/problems/boolfind/input_validators/validate.py
+++ test/problems/boolfind/input_validators/validate.py
@@ -14,9 +14,13 @@ try:
         if not re.match(r"[1-9][0-9]*\n", line):
             sys.exit(43)
         n = int(line)
-        for _ in range(n):
+        if n < 2:
+            sys.exit(43)
+        for i in range(n):
             line = sys.stdin.readline()
             if not re.match(r"(0|1)\n", line):
+                sys.exit(43)
+            if i == 0 and line[0] == "0" or i == n - 1 and line[0] == "1":
                 sys.exit(43)
 except UnicodeDecodeError:
     sys.exit(43)
