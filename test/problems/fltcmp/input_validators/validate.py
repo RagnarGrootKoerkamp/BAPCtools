556469d4d9673f6a1b9d6433a1836d69c291c7a4
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-01-17 21:38:38 +0100

    Fix input validator for fltcmp

--- test/problems/fltcmp/input_validators/validate.py
+++ test/problems/fltcmp/input_validators/validate.py
@@ -7,13 +7,13 @@ line = sys.stdin.readline()
 if not re.match("([1-9][0-9]*)\n", line):
     print("Positive integer expected", file=sys.stderr)
     sys.exit(43)
-    n = int(line)
-    for _ in range(n):
-        line = sys.stdin.readline()
-        try:
-            x = float(line)
-        except ValueError:
-            print("Float expected, got {x}", file=sys.stderr)
-            sys.exit(43)
+n = int(line)
+for _ in range(n):
+    line = sys.stdin.readline()
+    try:
+        x = float(line)
+    except ValueError:
+        print("Float expected, got {x}", file=sys.stderr)
+        sys.exit(43)
 sys.exit(42)
 
