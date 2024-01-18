680b29f76fc9d5d2dc00c7f384c4dc7c593cd43d
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-01-18 07:43:15 +0100

    fltcmp: Answer validator that also inspects .in-file (for n)

--- test/problems/fltcmp/answer_validators/validate.py
+++ test/problems/fltcmp/answer_validators/validate.py
@@ -2,10 +2,14 @@
 
 import sys
 
-for line in sys.stdin:
-    try:
-        x = float(line)
-    except ValueError:
-        print("Float expected", file=sys.stderr)
-        sys.exit(43)
+with open(sys.argv[1]) as infile:
+    for _ in range(int(infile.readline())):
+        line = sys.stdin.readline()
+        try:
+            x = float(line)
+        except ValueError:
+            print("Float expected", file=sys.stderr)
+            sys.exit(43)
+if not sys.stdin.readline() == "":
+    sys.exit(43)
 sys.exit(42)
