5ea16da5338f6d09bdf986f79e79fce30005414f
mzuenni <michi.zuendorf@gmail.com>
2024-03-08 16:41:25 +0100

    add new variable

--- bin/fuzz.py
+++ bin/fuzz.py
@@ -156,7 +156,7 @@ class Fuzz:
                 t.in_is_generated
                 and t.generator.uses_seed
                 and t.parse_error is None
-                and t.count_index == 0
+                and t.count_index == 0  # all rules with higher count index are duplicates anyway
             ):
                 self.testcase_rules.append(t)
 
