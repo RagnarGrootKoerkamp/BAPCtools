cedc237694aa73ae9de05437403eb4796b85eef2
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-03-10 10:21:19 +0100

    Add divsort to pytest

--- test/test_problems.py
+++ test/test_problems.py
@@ -10,7 +10,7 @@ import config
 import util
 
 # Run `bt run` on these problems.
-PROBLEMS = ['hello', 'helloproblemtools', 'different', 'fltcmp', 'boolfind', 'guess'] + [
+PROBLEMS = ['hello', 'helloproblemtools', 'different', 'fltcmp', 'boolfind', 'guess', 'divsort'] + [
     'hellounix' if not util.is_mac() and not util.is_windows() else []
 ]
 
