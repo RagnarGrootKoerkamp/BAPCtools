b7aa8021f8b843efff2d1bec3b96cef14ea757d7
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-03-12 12:30:47 +0100

    Yield (in amort. constant time) next unknown child verdict

--- test/test_verdicts.py
+++ test/test_verdicts.py
@@ -20,9 +20,12 @@ class TestVerdicts:
     def test_first_error(self):
         verds = verdicts.Verdicts(PATHS)
         assert len(verds.testgroups) == 5
+        assert verds.num_unknowns["secret/a"] == 3
         assert verds.set("secret/a/1", AC) == "secret/a/1"
         assert verds.set("secret/a/3", WA) == "secret/a/3"
         assert verds.verdicts["secret/a"] is None
+        assert verds.first_unknown["secret/a"] == "secret/a/2"
+        assert verds.num_unknowns["secret/a"] == 1
         assert verds.set("secret/a/2", WA) == "secret"
         assert verds.verdicts["secret/a"] == WA
         assert verds.verdicts["secret"] == WA
@@ -30,3 +33,15 @@ class TestVerdicts:
         verds.set("sample/1", AC)
         verds.set("sample/2", AC)
         assert verds.verdicts["."] == WA
+
+    def test_efficiency(self):
+        # If done badly, this takes quadratic time
+        size = 100000
+        many_paths = [f"a/{i}" for i in range(size)]
+        verds = verdicts.Verdicts(many_paths)
+        evens = range(0, size, 2)
+        odds = range(1, size, 2)
+        for i in reversed(evens):
+            verds.set(f"a/{i}", AC)
+        for i in (odds):
+            verds.set(f"a/{i}", AC)
