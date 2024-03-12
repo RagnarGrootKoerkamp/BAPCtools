501c70500ec54c10e086bbc23f3f52af0ab5003c
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-03-12 08:29:21 +0100

    Use str, not pathlib.Path for testnode identifiers

--- test/test_verdicts.py
+++ test/test_verdicts.py
@@ -1,5 +1,4 @@
 import verdicts
-from pathlib import Path
 
 AC = verdicts.Verdict.ACCEPTED
 WA = verdicts.Verdict.WRONG_ANSWER
@@ -8,26 +7,26 @@ PATHS = ["sample/1", "sample/2", "secret/a/1", "secret/a/2", "secret/a/3", "secr
 
 class TestVerdicts:
     def test_inherited_inference(self):
-        verds = verdicts.Verdicts(Path(p) for p in PATHS)
+        verds = verdicts.Verdicts(PATHS)
         assert len(verds.testgroups) == 5
-        assert Path("secret/a/1") == verds.set(Path("secret/a/1"), AC)
-        assert Path("secret/a/2") == verds.set(Path("secret/a/2"), AC)
-        assert Path("secret/a") == verds.set(Path("secret/a/3"), AC)
-        assert Path("secret") == verds.set(Path("secret/b/1"), AC)
-        assert Path("sample/1") == verds.set(Path("sample/1"), AC)
-        assert verds.verdicts[Path(".")] is None
-        assert Path(".") == verds.set(Path("sample/2"), AC)
+        assert verds.set("secret/a/1", AC)  == "secret/a/1"
+        assert verds.set("secret/a/2", AC)  == "secret/a/2"
+        assert verds.set("secret/a/3", AC)  == "secret/a"
+        assert verds.set("secret/b/1", AC)  == "secret"
+        assert verds.set("sample/1", AC)  == "sample/1"
+        assert verds.verdicts["."] is None
+        assert verds.set("sample/2", AC) == '.'
 
     def test_first_error(self):
-        verds = verdicts.Verdicts(Path(p) for p in PATHS)
+        verds = verdicts.Verdicts(PATHS)
         assert len(verds.testgroups) == 5
-        assert Path("secret/a/1") == verds.set(Path("secret/a/1"), AC)
-        assert Path("secret/a/3") == verds.set(Path("secret/a/3"), WA)
-        assert verds.verdicts[Path("secret/a")] is None
-        assert Path("secret") == verds.set(Path("secret/a/2"), WA)
-        assert verds.verdicts[Path("secret/a")] == WA
-        assert verds.verdicts[Path("secret")] == WA
-        assert verds.verdicts[Path(".")] is None
-        verds.set(Path("sample/1"), AC)
-        verds.set(Path("sample/2"), AC)
-        assert verds.verdicts[Path(".")] == WA
+        assert verds.set("secret/a/1", AC) == "secret/a/1"
+        assert verds.set("secret/a/3", WA) == "secret/a/3"
+        assert verds.verdicts["secret/a"] is None
+        assert verds.set("secret/a/2", WA) == "secret"
+        assert verds.verdicts["secret/a"] == WA
+        assert verds.verdicts["secret"] == WA
+        assert verds.verdicts["."] is None
+        verds.set("sample/1", AC)
+        verds.set("sample/2", AC)
+        assert verds.verdicts["."] == WA
