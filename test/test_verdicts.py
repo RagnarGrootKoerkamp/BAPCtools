662371d71a32d45e252c98f2d01064d817ecc2ae
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-03-13 08:15:26 +0100

    test that an internal node does not get written twice with inferred verdict

--- test/test_verdicts.py
+++ test/test_verdicts.py
@@ -43,3 +43,10 @@ class TestVerdicts:
             verds.set(f"a/{i}", AC)
         for i in odds:
             verds.set(f"a/{i}", AC)
+
+    def test_parent_overwrite(self):
+        # If implmented badly, will overwrite verdict at `secret/a' (and crash)
+        verds = verdicts.Verdicts(PATHS)
+        verds.set("secret/a/1", WA)
+        assert verds.verdict['secret/a'] == WA
+        verds.set("secret/a/2", WA) # should not try to write 'sexret/a' again
