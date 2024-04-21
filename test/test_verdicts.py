5cefd5dc2c1c1965bc7b39f6ffa6905dfaa43424
Maarten Sijm <9739541+mpsijm@users.noreply.github.com>
2024-04-21 13:38:23 +0200

    Fix test/test_verdicts.py

--- test/test_verdicts.py
+++ test/test_verdicts.py
@@ -8,47 +8,57 @@ PATHS = ["sample/1", "sample/2", "secret/a/1", "secret/a/2", "secret/a/3", "secr
 class TestVerdicts:
     def test_inherited_inference(self):
         verds = verdicts.Verdicts(PATHS)
-        verds["secret/a/1"] = AC
-        verds["secret/a/2"] = AC
-        verds["secret/a/3"] = AC
-        verds["secret/c"] = AC
-        verds["secret/b/1"] = AC
-        verds["sample/1"] = AC
+        verds.set("secret/a/1", AC, 0.5)
+        verds.set("secret/a/2", AC, 0.5)
+        verds.set("secret/a/3", AC, 0.5)
+        verds.set("secret/c", AC, 0.5)
+        verds.set("secret/b/1", AC, 0.5)
+        verds.set("sample/1", AC, 0.5)
         assert verds["."] is None
-        verds["sample/2"] = AC
+        verds.set("sample/2", AC, 0.5)
         assert verds["."] == AC
 
     def test_first_error(self):
         verds = verdicts.Verdicts(PATHS)
-        assert verds.num_unknowns["secret/a"] == 3
-        verds["secret/a/1"] = AC
-        verds["secret/a/3"] = WA
+        assert all(verds.run_is_needed(f"secret/a/{i}") for i in range(1, 3))
+        verds.set("secret/a/1", AC, 0.5)
+        verds.set("secret/a/3", WA, 0.5)
         assert verds["secret/a"] is None
-        assert verds.first_unknown["secret/a"] == "secret/a/2"
-        assert verds.num_unknowns["secret/a"] == 1
-        verds["secret/a/2"] = WA
+        assert verds.run_is_needed("secret/a/2")
+        verds.set("secret/a/2", WA, 0.5)
         assert verds["secret/a"] == WA
         assert verds["secret"] == WA
         assert verds["."] is None
-        verds["sample/1"] = AC
-        verds["sample/2"] = AC
+        verds.set("sample/1", AC, 0.5)
+        verds.set("sample/2", AC, 0.5)
         assert verds["."] == WA
 
     def test_efficiency(self):
-        # If done badly, this takes quadratic time
-        size = 100000
+        # Setting a verdict takes linear time: it checks the verdicts of all siblings to determine the parent's verdict.
+        # This means that this test_efficiency() runs in quadratic time.
+        size = 1000
         many_paths = [f"a/{i}" for i in range(size)]
         verds = verdicts.Verdicts(many_paths)
         evens = range(0, size, 2)
         odds = range(1, size, 2)
         for i in reversed(evens):
-            verds[f"a/{i}"] = AC
+            verds.set(f"a/{i}", AC, 0.5)
         for i in odds:
-            verds[f"a/{i}"] = AC
+            verds.set(f"a/{i}", AC, 0.5)
 
     def test_parent_overwrite(self):
-        # If implmented badly, will overwrite verdict at `secret/a' (and crash)
+        # If implemented badly, will overwrite verdict at `secret/a' (and crash)
         verds = verdicts.Verdicts(PATHS)
-        verds["secret/a/1"] = WA
-        assert verds['secret/a'] == WA
-        verds["secret/a/2"] = WA  # should not try to write 'sexret/a' again
+        verds.set("secret/a/1", WA, 0.5)
+        assert verds["secret/a"] == WA
+        verds.set("secret/a/2", WA, 0.5)  # should not try to write 'secret/a' again
+
+    def test_slowest_testcase(self):
+        verds = verdicts.Verdicts(PATHS, verdicts.RunUntil.DURATION, 3)
+        verds.set("sample/1", AC, 0.5)
+        verds.set("sample/2", AC, 0.5)
+        verds.set("secret/a/1", "TLE", 2.9)
+        verds.set("secret/a/2", "RTE", 3.5)
+        verds.set("secret/a/3", "TLE", 3.2)
+        assert verds.salient_testcase() == ("secret/a/1", 2.9)
+        assert verds.slowest_testcase() == ("secret/a/2", 3.5)
