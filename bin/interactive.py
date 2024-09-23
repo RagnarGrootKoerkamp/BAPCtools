RagnarGrootKoerkamp/BAPCtools

commit 7918f67c2faffba712d0e5c93f9b37d9c1f9d333

add validator_pass limit

bin/interactive.py
@@ -84,11 +84,11 @@ def run_interactive_testcase(
         if team_error is False:
             team_error = subprocess.PIPE
 
-        last_pass = 0
+        pass_id = 0
         max_duration = 0
         tle_result = None
         while True:
-            last_pass += 1
+            pass_id += 1
             # Start the validator.
             validator_command = get_validator_command()
             validator_process = subprocess.Popen(
@@ -137,7 +137,7 @@ def run_interactive_testcase(
                         _feedback(run, validator_err),
                         exec_res.err,
                         verdict,
-                        last_pass if run.problem.multipass else None,
+                        pass_id if run.problem.multipass else None,
                     )
                 else:
                     tle_result.timeout_expired |= max_duration >= timeout
@@ -155,6 +155,10 @@ def run_interactive_testcase(
                 break
             elif not run._prepare_nextpass(nextpass):
                 break
+            elif pass_id >= self.problem.limits.validation_passes:
+                bar.error(f'exceeded limit of validation_passes', resume=True)
+                verdict = Verdict.VALIDATOR_CRASH
+                break
 
         if tle_result is None:
             # Set result.err to validator error and result.out to team error.
@@ -166,7 +170,7 @@ def run_interactive_testcase(
                 _feedback(run, validator_err),
                 exec_res.err,
                 verdict,
-                last_pass if run.problem.multipass else None,
+                pass_id if run.problem.multipass else None,
             )
         else:
             tle_result.duration = max_duration
@@ -213,11 +217,11 @@ while True:
     new = l=='\n'
 '''
 
-    last_pass = 0
+    pass_id = 0
     max_duration = 0
     tle_result = None
     while True:
-        last_pass += 1
+        pass_id += 1
         validator_command = get_validator_command()
         validator = subprocess.Popen(
             validator_command,
@@ -406,7 +410,7 @@ while True:
                     val_err,
                     team_err,
                     verdict,
-                    last_pass if run.problem.multipass else None,
+                    pass_id if run.problem.multipass else None,
                 )
             else:
                 tle_result.timeout_expired |= aborted
@@ -414,6 +418,10 @@ while True:
         if not verdict and not self._continue_with_tle(verdict, aborted):
             break
         elif not run._prepare_nextpass(nextpass):
+            break
+        elif pass_id >= self.problem.limits.validation_passes:
+            bar.error(f'exceeded limit of validation_passes', resume=True)
+            verdict = Verdict.VALIDATOR_CRASH
             break
 
         if interaction:
@@ -431,7 +439,7 @@ while True:
             val_err,
             team_err,
             verdict,
-            last_pass if run.problem.multipass else None,
+            pass_id if run.problem.multipass else None,
         )
     else:
         tle_result.duration = max_duration

bin/problem.py
@@ -116,6 +116,7 @@ class Problem:
             'validator_flags': [],
             'author': '',
             'uuid': None,
+            'limits': {},
         }
 
         yaml_path = self.path / 'problem.yaml'
@@ -175,6 +176,22 @@ class Problem:
             raw += f'\n# uuid added by BAPCtools\nuuid: {self.settings.uuid}\n'
             yaml_path.write_text(raw)
             log(f'Generated UUID for {self.name}, added to problem.yaml')
+
+        # TODO: implement other limits
+        # TODO move timelimit to this?
+        self.limits = {}
+
+        for k, v in self.settings.limits.items():
+            self.limits[k] = v
+
+        has_validation_passes = 'validation_passes' in self.limits
+        if self.multipass and not has_validation_passes:
+            self.limits['validation_passes'] = 2
+        if not self.multipass and has_validation_passes:
+            self.limits.pop('validation_passes')
+            warn('limit: validation_passes is only used for multipass problems. SKIPPED.')
+
+        self.limits = argparse.Namespace(**self.limits)
 
     def _parse_testdata_yaml(p, path, bar):
         assert path.is_relative_to(p.path / 'data')

bin/run.py
@@ -52,11 +52,11 @@ class Run:
                 assert not interaction.is_relative_to(self.tmpdir)
                 interaction = interaction.open('a')
             nextpass = self.feedbackdir / 'nextpass.in' if self.problem.multipass else False
-            last_pass = 0
+            pass_id = 0
             max_duration = 0
             tle_result = None
             while True:
-                last_pass += 1
+                pass_id += 1
                 result = self.submission.run(self.in_path, self.out_path)
                 max_duration = max(max_duration, result.duration)
 
@@ -78,7 +78,7 @@ class Run:
                     result.verdict = Verdict.TIME_LIMIT_EXCEEDED
                     if tle_result is None:
                         tle_result = result
-                        tle_result.pass_id = last_pass if self.problem.multipass else None
+                        tle_result.pass_id = pass_id if self.problem.multipass else None
                     else:
                         tle_result.timeout_expired |= result.timeout_expired
                     if not self._continue_with_tle(result.verdict, result.timeout_expired):
@@ -119,6 +119,10 @@ class Run:
 
                 if not self._prepare_nextpass(nextpass):
                     break
+                elif pass_id >= self.problem.limits.validation_passes:
+                    bar.error(f'exceeded limit of validation_passes', resume=True)
+                    result.verdict = Verdict.VALIDATOR_CRASH
+                    break
 
                 if interaction:
                     print('---', file=interaction)
@@ -127,7 +131,7 @@ class Run:
                 interaction.close()
 
             if self.problem.multipass:
-                result.pass_id = last_pass
+                result.pass_id = pass_id
 
             if tle_result is not None:
                 result = tle_result

commit c91b624fd743d0c62ff52a39a97877cbc5c25956

#102 Fix type errors introduced in 7918f67c2faf

bin/interactive.py
@@ -158,7 +158,7 @@ def run_interactive_testcase(
             elif not run._prepare_nextpass(nextpass):
                 break
             elif pass_id >= run.problem.limits.validation_passes:
-                bar.error(f'exceeded limit of validation_passes', resume=True)
+                error('exceeded limit of validation_passes')
                 verdict = Verdict.VALIDATOR_CRASH
                 break
 
@@ -422,7 +422,7 @@ while True:
         elif not run._prepare_nextpass(nextpass):
             break
         elif pass_id >= run.problem.limits.validation_passes:
-            bar.error(f'exceeded limit of validation_passes', resume=True)
+            error('exceeded limit of validation_passes')
             verdict = Verdict.VALIDATOR_CRASH
             break
 

bin/problem.py
@@ -179,19 +179,16 @@ class Problem:
 
         # TODO: implement other limits
         # TODO move timelimit to this?
-        self.limits = {}
+        limits = self.settings.limits.copy()
 
-        for k, v in self.settings.limits.items():
-            self.limits[k] = v
-
-        has_validation_passes = 'validation_passes' in self.limits
+        has_validation_passes = 'validation_passes' in limits
         if self.multipass and not has_validation_passes:
-            self.limits['validation_passes'] = 2
+            limits['validation_passes'] = 2
         if not self.multipass and has_validation_passes:
-            self.limits.pop('validation_passes')
+            limits.pop('validation_passes')
             warn('limit: validation_passes is only used for multipass problems. SKIPPED.')
 
-        self.limits = argparse.Namespace(**self.limits)
+        self.limits = argparse.Namespace(**limits)  # TODO #102 typed Namespace?
 
     def _parse_testdata_yaml(p, path, bar):
         assert path.is_relative_to(p.path / 'data')