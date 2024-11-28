4f65aad80565c34c6d1fe972fdff08cc01b4ab47
Thore Husfeldt <thore.husfeldt@gmail.com>
2024-11-28 19:42:27 +0100

    Many positive and negative schema tests for generators.yaml

--- test/generator_yaml/test_schemata.sh
+++ test/generator_yaml/test_schemata.sh
@@ -1,34 +1,98 @@
+# Validate all valid generator YAML found in the following dirs agains the CUE schema:
+
+all_valid_yaml=(../../doc ../../skel/problem ../problems valid_yaml)
+
+# Arguments
+#
+#  -j: also validate the JSON schema
+#
+# Also make sure invalide YAML in invalid_yaml/*generators.yaml is rejected;
+# here the yaml can be split using `---` and each snippet is individually 
+# validated.
+
 schemadir="../../support/schemas"
-all_valid_yaml=(../../doc ../../skel/problem/ ../problems valid_yaml)
 
 any_failed=0
 
-# Iterate over each .yaml file in the directory
+SNIPPETS_DIR=$(mktemp -d)
+trap "rm -rf $SNIPPETS_DIR" EXIT
+
 for dir in "${all_valid_yaml[@]}"; do
     for file in $(find "$dir" -type f -name '*generators.yaml'); do
-	    echo -n "Processing $file... "
-	    output=$(cue vet "$file" $schemadir/*.cue -d "#Generators" 2>&1)
-	    exit_code=$?
-	    if [ $exit_code -eq 0 ]; then
-		    echo -e "\033[0;32mOK\033[0m"
+	    echo -n "cue vet "$file" $schemadir/*.cue -d \"#Generators\" "
+	    output_cue=$(cue vet "$file" $schemadir/*.cue -d "#Generators" 2>&1)
+	    exit_code_cue=$?
+	    if [ $exit_code_cue -eq 0 ]; then
+		    echo -n -e "\033[0;32mOK(cue)\033[0m"
 	    else
-		    echo -e "\033[0;31mError\033[0m"
-		    echo "Output from cue vet:"
-		    echo "$output"
+		    echo -n -e "\033[0;31mError(cue)\033[0m"
 		    any_failed=1
-		    fi	
+	    fi
+
+	    output_json=""
+	    if [ "$1" = "-j" ]; then
+		    output_json=$(cue vet "$file" $schemadir/generators_yaml_schema.json 2>&1)
+		    exit_code_json=$?
+		    if [ $exit_code_json -eq 0 ]; then
+			    echo -n -e "\033[0;32m OK(json)\033[0m"
+		    else
+			    echo -n -e "\033[0;31m Error(json)\033[0m"
+			    any_failed=1
+		    fi
+	    fi
+	    echo "$output_cue" | head -n 1
+	    if [ -n "$output_json" ]; then
+		    echo "$output_json" | head -n 1
+	    fi
 	    done
 done
-for file in invalid_yaml/*generators.yaml; do
-    echo -n "Processing $file... "
-    output=$(cue vet "$file" $schemadir/*.cue -d "#Generators" 2>&1)
-    exit_code=$?
-    if [ $exit_code -eq 1 ]; then
-	    echo -e "\033[0;32mOK (correctly rejected)\033[0m"
-    else
-	    echo -e "\033[0;31mIncorrectly accepted\033[0m"
-	    any_failed=1
-    fi	
+for yamlfile in invalid_yaml/*generators.yaml; do
+	if grep -q '^---$' "$yamlfile"; then
+		echo "Processing $yamlfile as snippets..."
+		# Split the YAML file into snippets based on `---`
+		awk -v snippets_dir="$SNIPPETS_DIR" -v file_base="$(basename "$yamlfile")" '
+			BEGIN {
+			    snippet_count = 0
+			    snippet_file = snippets_dir "/" file_base "_snippet_" (++snippet_count) ".yaml"
+			}
+			/^---/ {
+			    if (snippet_file != "") close(snippet_file)
+			    snippet_file = snippets_dir "/" file_base "_snippet_" (++snippet_count) ".yaml"
+			}
+			$0 !~ /^---/ { print > snippet_file }
+			END { if (snippet_file != "") close(snippet_file) }
+		' "$yamlfile"
+	else
+		cp "$yamlfile" "$SNIPPETS_DIR/$(basename "$yamlfile")"
+	fi
+done
+
+# Run `cue vet` on each invalid yaml file and snippet
+for snippet in "$SNIPPETS_DIR"/*.yaml; do
+	echo -n "Invalidating $(basename $snippet) "
+	snippet_failed=0
+	cue vet "$snippet"  $schemadir/*.cue -d "#Generators" > /dev/null 2>&1
+	if [[ $? -ne 0 ]]; then
+		echo -n -e "\033[0;32mOK (correctly rejected)\033[0m"
+	else
+		echo -n -e "\033[0;31mIncorrectly accepted\033[0m"
+		snippet_failed=1
+	fi
+	if [ "$1" = "-j" ]; then
+		cue vet $snippet $schemadir/generators_yaml_schema.json > /dev/null 2>&1
+		exit_code_json=$?
+		if [ $exit_code_json -ne 0 ]; then
+			echo -n -e "\033[0;32m OK(json)\033[0m"
+		else
+			echo -n -e "\033[0;31m Error(json)\033[0m"
+			snipped_failed=1
+		fi
+	fi
+	printf "\n"
+	if [ $snippet_failed = 1 ]; then
+		any_failed=1
+		cat $snippet
+	fi
 done
 if [ $any_failed -ne 0 ]; then
     echo "One or more cue vet commands failed." >&2
