be31eabd86f09c9eab4af97a37f6d5c9e24d7436
mzuenni <michi.zuendorf@gmail.com>
2024-12-18 11:47:07 +0100

    fix empty union

--- bin/stats.py
+++ bin/stats.py
@@ -123,7 +123,8 @@ def problem_stats(problems):
 
         def count(path):
             if type(path) is list:
-                return set.union(*(count(p) for p in path))
+                paths = [*(count(p) for p in path)]
+                return set.union(*paths) if paths else set()
             if callable(path):
                 return path(generated_testcases)
             results: set[str | Path] = set()
