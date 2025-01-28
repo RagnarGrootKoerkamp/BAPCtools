db71e7fa262b317479f1129cfe36d969ba79ba46
mzuenni <michi.zuendorf@gmail.com>
2025-01-28 15:52:24 +0100

    improve code

--- bin/interactive.py
+++ bin/interactive.py
@@ -46,7 +46,7 @@ def run_interactive_testcase(
 
     timelimit = run.problem.settings.timelimit
     timeout = run.problem.settings.timeout
-    memory = run.submission.memory
+    memory = run.problem.limits.memory
 
     # Validator command
     def get_validator_command():
