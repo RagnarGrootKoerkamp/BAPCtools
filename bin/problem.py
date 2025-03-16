22980eb78dd72a266b88af01fc7d675602f5ec13
Maarten Sijm <9739541+mpsijm@users.noreply.github.com>
2025-03-16 13:29:04 +0100

    [validate] Add OutputVisualizer as special type of Validator

--- bin/problem.py
+++ bin/problem.py
@@ -835,6 +835,9 @@ class Problem:
                 paths = [problem.path / validate.OutputValidator.source_dir]
             else:
                 paths = [config.TOOLS_ROOT / "support" / "default_output_validator.cpp"]
+        elif cls == validate.OutputVisualizer:
+            # TODO: if not config.args.no_output_visualizer:
+            paths = [problem.path / validate.OutputVisualizer.source_dir]
         else:
             assert hasattr(cls, "source_dirs")
             paths = [
@@ -872,7 +875,7 @@ class Problem:
             )
             for path in paths
         ]
-        bar = ProgressBar(f"Building {cls.validator_type} validator", items=validators)
+        bar = ProgressBar(f"Building {cls.validator_type}", items=validators)
 
         def build_program(p):
             localbar = bar.start(p)
@@ -891,9 +894,10 @@ class Problem:
         if not testcases:
             return False
 
-        # Pre build the output validator to prevent nested ProgressBars.
+        # Pre build the output validator and visualizer to prevent nested ProgressBars.
         if not problem.validators(validate.OutputValidator):
             return False
+        problem.validators(validate.OutputVisualizer)
 
         submissions = problem.submissions()
         if not submissions:
