abfa1c000e8cd725f88530e063094e751703f4b5
mzuenni <michi.zuendorf@gmail.com>
2025-05-14 16:05:54 +0200

    test

--- test/problems/hellounix/submissions/run_time_error/test-memsize_512.cc
+++ test/problems/hellounix/submissions/run_time_error/test-memsize_512.cc
@@ -13,9 +13,26 @@ using namespace std;
 
 const size_t mb = 513;
 
+template<typename T>
+T use(std::vector<T>& todo) {
+	// init with some data
+	for (std::size_t i = 0; i < todo.size(); i++) todo[i] = T(i % 7);
+	// do some computation that needs the memory
+	for (int k = 0; k < 7; k++) {
+		for (std::size_t i = 0; i < todo.size(); i++) {
+			std::size_t j = (i + todo[i]) % todo.size();
+			todo[j] = (todo[i] % 7) + (todo[j] % 7);
+		}
+	}
+	// accumulate the result
+	T res = 0;
+	for (auto x : todo) res = (res >> 1) + (x >> 1);
+	return res;
+}
+
 int main() {
-	std::cerr << "Trying to allocate at least: " << 513 << " MB" << std::endl;
-	vector<char> v(513 * 1024 * 1024);
-	std::cerr << "Allocated: " << (v.end() - v.begin()) << " MB" << std::endl;
+	std::cerr << "Trying to allocate at least: " << mb << " MB" << std::endl;
+	vector<char> v(mb * 1024 * 1024);
+	std::cerr << "Allocated: " << mb << " MB (" << use(v) << ")" << std::endl;
 	return 0;
 }
