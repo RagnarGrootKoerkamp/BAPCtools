8fb4d20b94d386a2943d14d0f516cc89b6a18c45
mzuenni <michi.zuendorf@gmail.com>
2025-12-26 03:22:40 +0100

    update name regex

--- bapctools/config.py
+++ bapctools/config.py
@@ -46,7 +46,7 @@ KNOWN_LICENSES: Final[Sequence[str]] = (
 # When --table is set, this threshold determines the number of identical profiles needed to get flagged.
 TABLE_THRESHOLD: Final[int] = 4
 
-FILE_NAME_REGEX: Final[str] = "[a-zA-Z0-9][a-zA-Z0-9_.-]{0,253}[a-zA-Z0-9]"
+FILE_NAME_REGEX: Final[str] = "[a-zA-Z0-9_][a-zA-Z0-9_.-]{0,254}"
 COMPILED_FILE_NAME_REGEX: Final[re.Pattern[str]] = re.compile(FILE_NAME_REGEX)
 
 CONSTANT_NAME_REGEX = "[a-zA-Z_][a-zA-Z0-9_]*"
