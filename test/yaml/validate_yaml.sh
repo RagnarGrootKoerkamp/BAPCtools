6d543df51c1e7090a570d7066f427c2117335eac
Thore Husfeldt <thore.husfeldt@gmail.com>
2025-12-31 09:59:34 +0100

    Add test suite for test_group.yaml

--- test/yaml/validate_yaml.sh
+++ test/yaml/validate_yaml.sh
@@ -7,8 +7,8 @@ cd "$(dirname "$0")"
 # Configuration
 # ----------------------------
 
-all_valid_yaml=(../../doc ../problems problem/valid_yaml generators/valid_yaml)
-all_invalid_yaml=(generators/invalid_yaml problem/invalid_yaml)
+all_valid_yaml=(../../doc ../problems problem/valid_yaml generators/valid_yaml test_group/valid_yaml)
+all_invalid_yaml=(generators/invalid_yaml problem/invalid_yaml test_group/invalid_yaml)
 schemadir="../../bapctools/resources/support/schemas"
 
 
