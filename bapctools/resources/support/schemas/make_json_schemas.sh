1b2e773c25ff212a3d59bbf551b08a481db1c771
Thore Husfeldt <thore.husfeldt@gmail.com>
2026-01-01 20:37:02 +0100

    Add JSON versions of schemas for use with IDEs

--- bapctools/resources/support/schemas/make_json_schemas.sh
+++ bapctools/resources/support/schemas/make_json_schemas.sh
@@ -1,3 +1,5 @@
-cue export --out jsonschema -e '#Generators' \
-| sed -E 's/#([A-Za-z0-9_]+)/\1/g' \
-> test_schema.json
+# As of cue 0.15, we need to change #Definition to Definition to avoid convusing URI snippet syntax
+# Presumeably, this will change with a future CUE update, so the `sed` call will be redundant
+cue export --out jsonschema -e '#Generators' | sed -E 's/#([A-Za-z0-9_]+)/\1/g' > generators_schema.json
+cue export --out jsonschema -e '#Problem' | sed -E 's/#([A-Za-z0-9_]+)/\1/g' > problem_schema.json
+cue export --out jsonschema -e '#test_group' | sed -E 's/#([A-Za-z0-9_]+)/\1/g' > test_group_schema.json
