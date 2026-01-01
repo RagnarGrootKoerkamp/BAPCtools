84a0b4ac36f3352a4b62da47347b6ae9f08f965b
mzuenni <michi.zuendorf@gmail.com>
2026-01-01 14:23:05 +0100

    check message

--- bapctools/util.py
+++ bapctools/util.py
@@ -1252,6 +1252,15 @@ def math_eval(text: str) -> Optional[int | float]:
     return None
 
 
+def crop_line(output: str, limit: int) -> str:
+    output = output.strip()
+    if "\n" in output:
+        output = output.partition("\n")[0] + "[...]"
+    if len(output) > limit:
+        output = f"{output[: limit - 5]}[...]"
+    return output
+
+
 def crop_output(output: str) -> str:
     if config.args.error:
         return output
@@ -1268,7 +1277,7 @@ def crop_output(output: str) -> str:
     # Cap total length.
     if len(output) > 2000:
         output = output[:2000]
-        output += " ...\n"
+        output += "[...]\n"
         cropped = True
 
     if cropped:
