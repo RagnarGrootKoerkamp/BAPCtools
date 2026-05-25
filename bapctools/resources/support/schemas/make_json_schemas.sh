e6a392fff66ebe748a9f654e78dc3d0149c56e2d
Thore Husfeldt <thore.husfeldt@gmail.com>
2026-05-25 13:53:16 +0200

    Generate JSON schemas

--- bapctools/resources/support/schemas/make_json_schemas.sh
+++ bapctools/resources/support/schemas/make_json_schemas.sh
@@ -2,21 +2,14 @@
 set -euo pipefail
 
 CUE="${HOME}/go/bin/cue" # or just cue, but I need the most recent build
-# Change names #Foo and %23Foo to DEFFoo. This is because some LSP
-# implementations are confused about the URI fragment identifer syntax.
-SED_PATTERN='s/(#|%23)([A-Za-z0-9_]+)/DEF\2/g'
+
 
 export_schema() {
     local schema="$1"
     local output="$2"
 
     echo "Exporting $schema → $output"
-    "$CUE" export --out jsonschema -e "$schema" \
-	  | sed -E '
-	      s/^ {4}"additionalProperties"/    "unevaluatedProperties"/
-	      /^ {5,}"additionalProperties": false,?$/d
-	    ' \
-        > "$output"
+    "$CUE" export --out jsonschema -e "$schema" > "$output"
 }
 
 export_schema '#Generators'      generators_yaml_schema.json
